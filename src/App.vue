1f1a3ea2577a118d0bd880631d7180863d5e4843
Artur <artur.zajaczkowski76@gmail.com>
2022-07-15 15:23:06 +0200

    Add reaction mesure functionality

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
   <h1>My reaction timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
 
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
   components: {
+    Block
+  },
+  data(){
+    return{
+      isPlaying: false,
+      delay: null,
 
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    }
   }
 }
 </script>
---
+++ src/components/Block.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+    Click me
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  props: ['delay'],
+  data(){
+    return{
+      showBlock: false,
+      timer: null,
+      reactoinTime: 0
+    }
+  },
+  mounted(){
+    setTimeout(()=>{
+      this.showBlock = true
+      this.startTimer()
+    }, this.delay)
+  },
+  methods:{
+    startTimer(){
+      this.timer = setInterval(()=>{
+        this.reactoinTime += 10
+      }, 10)
+    },
+    stopTimer(){ 
+      clearInterval(this.timer)
+      console.log(this.reactoinTime)
+    },
+
+  }
+
+}
+</script>
+
+<style>
+.block{
+  width: 400px;
+  border-radius: 20px;
+  background-color: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
